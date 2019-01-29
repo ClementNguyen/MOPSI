b457122d6bbce2a1c63bc3785c9c35a03d538db4
sasha.c <collin.sasha@gmail.com>
2019-01-29 20:55:16 +0100

    yolo update

--- YOLO/image_similarity.py
+++ YOLO/image_similarity.py
@@ -1,6 +1,9 @@
 import os
 import subprocess
 import time
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
 
 dossier='/home/sasha/darknet'
 os.chdir(dossier)
@@ -21,12 +24,21 @@ def detect(image):
         names.append(result[k].split(': ')[0])
         percentages.append(result[k].split(': ')[1])
     print(names)
+    print(percentages)
     return(names,percentages)
 
 def similarite(image1,image2):
     time1=time.time()
     names1=detect(image1)[0]
+    img1=mpimg.imread("predictions.jpg")
     names2=detect(image2)[0]
+    img2=mpimg.imread("predictions.jpg")
+    plt.figure()
+    plt.imshow(img1)
+    plt.show()
+    plt.figure()
+    plt.imshow(img2)
+    plt.show()
     for i in range(len(names1)):
         for j in range(len(names2)):
             if names1[i]==names2[j] :
